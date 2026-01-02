20dc45d7df3bc167a182dd3d5bdd80419c1fa65b
nobitabot-sudo <skillbuddyxs@gmail.com>
2026-01-02 21:08:12 +0530

    Update ScamOverlayManager.kt

--- app/src/main/java/com/projectkavach/guardian/ScamOverlayManager.kt
+++ app/src/main/java/com/projectkavach/guardian/ScamOverlayManager.kt
@@ -1,8 +1,9 @@
 package com.projectkavach.guardian
 import android.content.Context
-import android.view.WindowManager
 
 class ScamOverlayManager(private val context: Context) {
-    fun showWarning() { /* Alert logic here */ }
-    fun hideWarning() { /* Hide logic here */ }
+    fun showShieldActive() {}
+    fun showScamAlert(scamType: String) {}
+    fun showCriticalAlert(scamType: String) {}
+    fun cleanup() {}
 }
