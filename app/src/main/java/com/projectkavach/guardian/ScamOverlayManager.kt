38151d1c8d3b593b485675ed0ac0c01f01ed1f27
nobitabot-sudo <skillbuddyxs@gmail.com>
2026-01-02 21:00:13 +0530

    Create ScamOverlayManager.kt

---
+++ app/src/main/java/com/projectkavach/guardian/ScamOverlayManager.kt
@@ -0,0 +1,8 @@
+package com.projectkavach.guardian
+import android.content.Context
+import android.view.WindowManager
+
+class ScamOverlayManager(private val context: Context) {
+    fun showWarning() { /* Alert logic here */ }
+    fun hideWarning() { /* Hide logic here */ }
+}
